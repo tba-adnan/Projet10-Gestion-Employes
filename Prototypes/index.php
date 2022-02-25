f80846f010cd388330b7ce86dd4d740c493c6d1d
tba-adnan <92032353+tba-adnan@users.noreply.github.com>
2022-02-25 11:51:42 +0100

    Add files via upload

---
+++ Prototypes/index.php
@@ -0,0 +1,55 @@
+<?php
+   include("admin-config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {  
+      $myusername = mysqli_real_escape_string($conn,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
+      $sql = "SELECT id FROM admindata WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($conn,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $count = mysqli_num_rows($result);
+      
+      if($count == 1) {
+         $_SESSION['login_user'] = $myusername;
+         header("location: app.php");
+      }else {
+        // ToAdd warning
+      }
+   }
+?>
+<html>
+   <head>
+      <title>admin Login Page</title>
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   <body bgcolor = "#FFFFFF">
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "padding:3px;"><b>Login</b></div>
+            <div style = "margin:30px">
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"></div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
